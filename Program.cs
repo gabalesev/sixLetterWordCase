d28ef7671027d118df1e73cf954b4b58e2cac229
Gabriel Sevilla <sevillagab@gmail.com>
2026-02-06 16:19:43 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using SixLetterWord;
+
+const int targetWordLength = 6;
+
+string? rootDir = Directory.GetParent(Directory.GetCurrentDirectory())?.Parent?.Parent?.FullName;
+string inputFilePath = Path.Combine(
+  rootDir ??
+  throw new DirectoryNotFoundException("Solution root directory not found."),
+  "input.txt");
+
+var words = await File.ReadAllLinesAsync(inputFilePath);
+
+var finder = new WordCombinationFinder(words, targetWordLength);
+var combinations = finder.FindCombinations();
+
+// results
+foreach (var combination in combinations)
+{
+    var parts = string.Join("+", combination);
+    var result = string.Concat(combination);
+    Console.WriteLine($"{parts}={result}");
+}
---
+++ WordCombinationFinder.cs
@@ -0,0 +1,64 @@
+﻿namespace SixLetterWord;
+
+public class WordCombinationFinder
+{
+    private readonly HashSet<string> _wordSet;
+    private readonly int _targetLength;
+
+    public WordCombinationFinder(IEnumerable<string> words, int targetLength = 6)
+    {
+        _targetLength = targetLength;
+        _wordSet = new HashSet<string>(words, StringComparer.OrdinalIgnoreCase);
+    }
+
+    public IEnumerable<List<string>> FindCombinations()
+    {
+        var targetWords = _wordSet.Where(w => w.Length == _targetLength);
+
+        foreach (var target in targetWords)
+        {
+            foreach (var combination in FindCombinationsForWord(target))
+            {
+                yield return combination;
+            }
+        }
+    }
+
+    private IEnumerable<List<string>> FindCombinationsForWord(string target)
+    {
+        var results = new List<List<string>>();
+        FindCombinationsRecursive(target, 0, [], results);
+        return results;
+    }
+
+    private void FindCombinationsRecursive(
+        string target,
+        int startIndex,
+        List<string> currentParts,
+        List<List<string>> results)
+    {
+        // Base case: we've matched the entire target word
+        if (startIndex == target.Length)
+        {
+            // Only valid if we used more than one word
+            if (currentParts.Count > 1)
+            {
+                results.Add(new List<string>(currentParts));
+            }
+            return;
+        }
+
+        // Try all possible prefix lengths from current position
+        for (int length = 1; length <= target.Length - startIndex; length++)
+        {
+            string prefix = target.Substring(startIndex, length);
+
+            if (_wordSet.Contains(prefix))
+            {
+                currentParts.Add(prefix);
+                FindCombinationsRecursive(target, startIndex + length, currentParts, results);
+                currentParts.RemoveAt(currentParts.Count - 1); // Backtrack
+            }
+        }
+    }
+}
