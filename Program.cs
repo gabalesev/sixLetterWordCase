f7b37bc2e444c3aea500f4876aa1543318f3deaf
Gabriel Sevilla <sevillagab@gmail.com>
2026-02-08 00:41:35 +0100

    Add todos comments. Improve time and space complexity

--- Program.cs
+++ Program.cs
@@ -1,15 +1,19 @@
-﻿using System.Collections.Generic;
-using SixLetterWord;
+﻿using SixLetterWord;
 
+// todo: read word length from command arguments 
 const int targetWordLength = 6;
+const string fileName = "input.txt";
 
 string? rootDir = Directory.GetParent(Directory.GetCurrentDirectory())?.Parent?.Parent?.FullName;
 string inputFilePath = Path.Combine(
   rootDir ??
   throw new DirectoryNotFoundException("Solution root directory not found."),
-  "input.txt");
+  fileName);
 
-var words = await File.ReadAllLinesAsync(inputFilePath);
+// todo: call asynchronously if the file is large
+var words = File.ReadLines(inputFilePath) // space complexity O(1)
+    .Where(line => !string.IsNullOrWhiteSpace(line))
+    .Select(line => line.Trim());
 
 var finder = new WordCombinationFinder(words, targetWordLength);
 var combinations = finder.FindCombinations();
--- WordCombinationFinder.cs
+++ WordCombinationFinder.cs
@@ -3,19 +3,36 @@
 public class WordCombinationFinder
 {
     private readonly HashSet<string> _wordSet;
+    private readonly HashSet<string> _targetWordSet;
     private readonly int _targetLength;
 
     public WordCombinationFinder(IEnumerable<string> words, int targetLength = 6)
     {
+        _wordSet = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
+        _targetWordSet = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
         _targetLength = targetLength;
-        _wordSet = new HashSet<string>(words, StringComparer.OrdinalIgnoreCase);
+
+        foreach (var word in words)
+        {
+            // skip words longer than target length
+            if (string.IsNullOrWhiteSpace(word) || word.Length > targetLength)
+                continue;
+
+            // pre-load words once
+            if (word.Length == targetLength)
+            {
+                _targetWordSet.Add(word);
+            }
+            else
+            {
+                _wordSet.Add(word);
+            }
+        }
     }
 
     public IEnumerable<List<string>> FindCombinations()
     {
-        var targetWords = _wordSet.Where(w => w.Length == _targetLength);
-
-        foreach (var target in targetWords)
+        foreach (var target in _targetWordSet)
         {
             foreach (var combination in FindCombinationsForWord(target))
             {
@@ -27,37 +44,40 @@ public class WordCombinationFinder
     private IEnumerable<List<string>> FindCombinationsForWord(string target)
     {
         var results = new List<List<string>>();
-        FindCombinationsRecursive(target, 0, [], results);
+        RecursiveCombinationsSearch(target, 0, [], results);
         return results;
     }
 
-    private void FindCombinationsRecursive(
+    private void RecursiveCombinationsSearch(
         string target,
         int startIndex,
         List<string> currentParts,
         List<List<string>> results)
     {
-        // Base case: we've matched the entire target word
+        // base case: match target word
         if (startIndex == target.Length)
         {
-            // Only valid if we used more than one word
-            if (currentParts.Count > 1)
-            {
-                results.Add(new List<string>(currentParts));
-            }
+            // no need to validate currentParts.Count > 1
+            // because we did not add the target words to _wordSet
+            results.Add(new List<string>(currentParts));
+
             return;
         }
 
-        // Try all possible prefix lengths from current position
-        for (int length = 1; length <= target.Length - startIndex; length++)
+        // save iterations by not starting from targetlength when startIndex is 0
+        int maxPrefixLength = Math.Min(_targetLength - 1, target.Length - startIndex);
+        for (int length = 1; length <= maxPrefixLength; length++)
         {
-            string prefix = target.Substring(startIndex, length);
+            // todo improvement: avoid string allocation
+            string substring = target.Substring(startIndex, length);
 
-            if (_wordSet.Contains(prefix))
+            if (_wordSet.Contains(substring)) // O(1) lookup
             {
-                currentParts.Add(prefix);
-                FindCombinationsRecursive(target, startIndex + length, currentParts, results);
-                currentParts.RemoveAt(currentParts.Count - 1); // Backtrack
+                // todo improvement: if a word has 2 or more times the same substring,
+                // validate if there are enough occurrences in the _wordSet for that substring
+                currentParts.Add(substring);
+                RecursiveCombinationsSearch(target, startIndex + length, currentParts, results);
+                currentParts.RemoveAt(currentParts.Count - 1);
             }
         }
     }
